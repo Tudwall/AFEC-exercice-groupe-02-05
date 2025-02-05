45b217eb829948ff7325ad794d86bbbfee420964
seliegarrigues <eliseg64@gmail.com>
2025-02-05 11:04:59 +0100

    maj de app.js : ajout de dotenv suppr export app et ajout app.listen

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 import express from "express";
-import produitRouter from "../AFEC-exercice-groupe-02-05/routes/produit.js";
+import dotenv from "dotenv";
+import produitRoutes from "./routes/produits.js";
+/* import seedProduits from"./" */
+
+dotenv.config();
 const app = express();
+const PORT = 3003;
 
 app.use(express.json());
-app.use("/produits", produitRouter);
+app.use("/produits", produitRoutes);
 
-export default app;
+app.listen(PORT, () => {
+  console.log(`le serveur tourne actuellement sur http://localhost:${PORT}`);
+});
