1047f3ae08d5ddc534f35b19257433f2908c1d9b
seliegarrigues <eliseg64@gmail.com>
2025-02-05 12:05:04 +0100

    ajout de user dans app.js et ajout d'une route users

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import produitRoutes from "./routes/produits.js";
+import userRoutes from "./routes/users.js";
 /* import seedProduits from"./" */
 
 dotenv.config();
@@ -9,6 +10,7 @@ const PORT = 3003;
 
 app.use(express.json());
 app.use("/produits", produitRoutes);
+app.use("/users", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`le serveur tourne actuellement sur http://localhost:${PORT}`);
